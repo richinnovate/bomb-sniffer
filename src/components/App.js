1deac0ff0947c34cbf4ed627e6a2d796c94ea0bd
TJ Alston <tjalston82@yahoo.com>
2016-08-25 11:50:29 -0400

    dinoboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,35 @@
 import React, { Component } from 'react'
+import Cell from './Cell'
 import '../styles/screen.sass'
 
 class App extends Component {
-
+  constructor () {
+    super()
+    this.state={
+      board: [
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' '],
+        [' ', '_' , 'F' , '*' , '@' , '3' , ' ' , ' ']
+      ]
+    }
+  }
   render () {
+    const rows = this.state.board.map((row, i) => {
+      const cells = row.map((cell, j) => {
+        return <Cell type={cell} key={j} />
+      })
+      return <tr key={i}>{cells}</tr>
+    })
     return <div>
       <h1>Bomb Sniffer!</h1>
       <table>
         <tbody>
-          <tr>
-            <td className="empty" />
-            <td className="flagged" />
-            <td className="unrevealed" />
-            <td className="bomb" />
-            <td className="numbered">3</td>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
-          <tr>
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-            <td className="empty" />
-          </tr>
+          {rows}
         </tbody>
       </table>
     </div>
---
+++ src/components/Cell.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+class Cell extends Component {
+  get _cellType () {
+    switch (this.props.type) {
+      case ' ': return 'unrevealed'
+      case '_': return 'empty'
+      case 'F': return 'flagged'
+      case '*': return 'bomb'
+      case '@': return 'flag-bomb'
+      default: return 'numbered'
+
+    }
+  }
+  render () {
+    return(<td className={this._cellType}>{this.props.type}</td>)
+  }
+}
+
+export default Cell
